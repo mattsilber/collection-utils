29faa301f63a078c00b1d25c6c9eac43b7985f42
Matt Silber <cruceo@guardanis.com>
2018-06-02 09:17:46 -0400

    Added base components for a sample application

---
+++ collection-utils-sample/src/main/java/com/guardanis/collections/sample/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.guardanis.collections.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.guardanis.collections.adapters.ModuleBuilder
+import com.guardanis.collections.recycler.ModularRecyclerView
+import com.guardanis.collections.recycler.adapters.ModularRecyclerAdapter
+import com.guardanis.collections.recycler.modules.EndlessModule
+import com.guardanis.collections.sample.modules.SampleTextModule
+import java.lang.ref.WeakReference
+
+class MainActivity: AppCompatActivity(), EndlessModule.EndlessEventListener {
+
+    private var recycler: WeakReference<ModularRecyclerView> = WeakReference<ModularRecyclerView>(null)
+    private lateinit var adapter: ModularRecyclerAdapter
+
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
+
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        appendRandomContent()
+    }
+
+    private fun setupRecyclerView() {
+        val recycler = findViewById<ModularRecyclerView>(R.id.main_list)
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recycler.registerModule(EndlessModule(this))
+
+        this.adapter = ModularRecyclerAdapter(this)
+
+        adapter.registerModuleBuilder(
+                SampleTextModule::class.java,
+                ModuleBuilder(
+                        R.layout.text_module,
+                        SampleTextModule.ViewModule::class.java,
+                        { SampleTextModule.ViewModule() }))
+
+        recycler.adapter = this.adapter
+
+        this.recycler = WeakReference(recycler)
+    }
+
+    private fun appendRandomContent() {
+        0.until(30)
+                .map({ SampleTextModule.createRandomInstance() })
+                .forEach({ adapter.add(it) })
+
+        adapter.notifyDataSetChanged()
+
+        recycler.get()
+                ?.getModule(EndlessModule::class.java)
+                ?.isLoading = false
+    }
+
+    override fun onNextPage() {
+        recycler.get()
+                ?.getModule(EndlessModule::class.java)
+                ?.isLoading = true
+
+        appendRandomContent()
+
+        if (1000 < adapter.itemCount)
+            recycler.get()
+                    ?.getModule(EndlessModule::class.java)
+                    ?.onEndingReached()
+    }
+}
---
+++ collection-utils-sample/src/main/java/com/guardanis/collections/sample/modules/SampleTextModule.kt
@@ -0,0 +1,75 @@
+package com.guardanis.collections.sample.modules
+
+import android.graphics.Color
+import android.view.View
+import android.support.annotation.ColorInt
+import android.support.v7.widget.RecyclerView
+import com.guardanis.collections.recycler.adapters.ModularRecyclerAdapter
+import com.guardanis.collections.recycler.adapters.RecyclerViewModule
+import com.guardanis.collections.sample.R
+import com.guardanis.fontutils.TextView
+import java.util.*
+
+class SampleTextModule(
+        private val title: String,
+        private val details: String,
+        private @ColorInt val backgroundColor: Int) {
+
+    class ViewModule(): RecyclerViewModule<SampleTextModule, ViewHolder>(R.layout.text_module) {
+
+        override fun buildViewHolder(view: View): ViewHolder {
+            return ViewHolder(view)
+        }
+
+        override fun updateView(adapter: ModularRecyclerAdapter, item: SampleTextModule, position: Int) {
+            viewHolder.titleView?.text = item.title
+            viewHolder.detailsView?.text = item.details
+            viewHolder.itemView.setBackgroundColor(item.backgroundColor)
+        }
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        var titleView: TextView?
+        var detailsView: TextView?
+
+        init {
+            this.titleView = view.findViewById(R.id.text_module_title)
+            this.detailsView = view.findViewById(R.id.text_module_details)
+        }
+    }
+
+    companion object {
+
+        private val randomTitle: String
+            get() {
+                return when (Random().nextInt(3)) {
+                    0 -> "This is a random title"
+                    1 -> "This is another title"
+                    else -> "This title is superior"
+                }
+            }
+
+        private val randomDetails: String
+            get() {
+                return when (Random().nextInt(3)) {
+                    0 -> "Sometimes random messages can be cool"
+                    1 -> "Other times they can be weirdly formatted,\nwith multiple lines and with useless information."
+                    else -> "I'm running out of ideas for random text"
+                }
+            }
+
+        private val randomColor: Int
+            get() {
+                return when (Random().nextInt(3)) {
+                    0 -> Color.RED
+                    1 -> Color.BLUE
+                    else -> Color.DKGRAY
+                }
+            }
+
+        fun createRandomInstance(): SampleTextModule {
+            return SampleTextModule(randomTitle, randomDetails, randomColor)
+        }
+    }
+}
