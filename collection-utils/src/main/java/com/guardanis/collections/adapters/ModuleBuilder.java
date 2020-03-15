ef386744bc95d2f98136b69c69099beca7901dc8
Matt Silber <cruceo@guardanis.com>
2020-03-15 08:52:36 -0400

    Marks the old ModuleBuilder construction with expecting a LayoutRes

--- collection-utils/src/main/java/com/guardanis/collections/adapters/ModuleBuilder.java
+++ collection-utils/src/main/java/com/guardanis/collections/adapters/ModuleBuilder.java
@@ -4,6 +4,7 @@ import com.guardanis.collections.adapters.builderdelegates.CompatModuleBuilderDe
 import com.guardanis.collections.adapters.builderdelegates.LayoutResourceModuleBuilderDelegate;
 import com.guardanis.collections.adapters.builderdelegates.ModuleBuilderDelegate;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 
 public class ModuleBuilder<T extends AdapterViewModule> {
@@ -11,7 +12,7 @@ public class ModuleBuilder<T extends AdapterViewModule> {
     protected ModuleBuilderDelegate<T> builderDelegate;
 
     public ModuleBuilder(
-            int layoutResId,
+            @LayoutRes int layoutResId,
             @NonNull final CompatModuleBuilderDelegate<T> builderDelegate) {
 
         this.builderDelegate = new LayoutResourceModuleBuilderDelegate<T>(layoutResId) {
