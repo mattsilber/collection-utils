10eac8e07cc28e888b0bd532dea1587d340cd2df
Matt Silber <matt@guardanis.com>
2022-09-24 13:44:40 -0400

    Reformats collection-utils-legacy

--- collection-utils-legacy/src/main/java/com/guardanis/collections/legacy/tools/ListUtils.java
+++ collection-utils-legacy/src/main/java/com/guardanis/collections/legacy/tools/ListUtils.java
@@ -1,5 +1,7 @@
 package com.guardanis.collections.legacy.tools;
 
+import androidx.annotation.NonNull;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,88 +12,97 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import androidx.annotation.NonNull;
-
 public class ListUtils<V> {
 
     public interface Filter<V> {
+
         public boolean isFilterMatched(V obj);
     }
 
     public interface Converter<V, T> {
+
         public T convert(V from);
     }
 
     public interface Zipper<V, T, U> {
+
         public U zip(V o1, T o2);
     }
 
-    public interface Reducer<T, V>{
+    public interface Reducer<T, V> {
+
         public T reduce(T last, V value);
     }
 
     public interface Action<V> {
+
         public void executeAction(V value);
     }
 
     private List<V> values;
 
-    public ListUtils(@NonNull V[] values){
+    public ListUtils(@NonNull V[] values) {
         this(Arrays.asList(values));
     }
 
-    public ListUtils(@NonNull List<V> values){
+    public ListUtils(@NonNull List<V> values) {
         this.values = values;
     }
 
-    public ListUtils(@NonNull Set<V> values){
+    public ListUtils(@NonNull Set<V> values) {
         this(values.toArray((V[]) new Object[values.size()]));
     }
 
     public <T> ListUtils<T> map(Converter<V, T> converter) {
         List<T> returnables = new ArrayList<T>();
 
-        for(V value : values)
+        for (V value : values) {
             returnables.add(converter.convert(value));
+        }
 
         return new ListUtils(returnables);
     }
 
     public ListUtils<Object> flatMap() {
-        return flatMap(new Converter<V, Object>(){
+        return flatMap(new Converter<V, Object>() {
             public Object convert(V from) {
                 return from;
             }
         });
     }
 
-    public <T, S> ListUtils<T> flatMap(Converter<S, T> converter){
+    public <T, S> ListUtils<T> flatMap(Converter<S, T> converter) {
         List<T> returnables = new ArrayList<T>();
 
         for (V value : values) {
             List<T> converted = new ArrayList<T>();
 
-            if(value instanceof List)
+            if (value instanceof List) {
                 converted.addAll(new ListUtils((List<V>) value)
-                        .flatMap(converter)
-                        .values());
-            else if(value instanceof Object[])
+                    .flatMap(converter)
+                    .values());
+            }
+            else if (value instanceof Object[]) {
                 converted.addAll(new ListUtils((V[]) value)
-                        .flatMap(converter)
-                        .values());
-            else if(value instanceof Set)
+                    .flatMap(converter)
+                    .values());
+            }
+            else if (value instanceof Set) {
                 converted.addAll(new ListUtils((Set<V>) value)
-                        .flatMap(converter)
-                        .values());
-            else if(value != null) {
+                    .flatMap(converter)
+                    .values());
+            }
+            else if (value != null) {
                 T item = converter.convert((S) value);
 
-                if(item != null)
+                if (item != null) {
                     converted.add(item);
+                }
             }
 
-            if(converted == null || converted.size() < 1)
+            if (converted == null || converted.size() < 1) {
                 continue;
+            }
 
             returnables.addAll(converted);
         }
@@ -99,89 +110,103 @@ public class ListUtils<V> {
         return new ListUtils(returnables);
     }
 
-    public ListUtils<V> filter(Filter<V> filter){
+    public ListUtils<V> filter(Filter<V> filter) {
         List<V> returnables = new ArrayList<V>();
 
-        for(V value : values)
-            if(filter.isFilterMatched(value))
+        for (V value : values) {
+            if (filter.isFilterMatched(value)) {
                 returnables.add(value);
+            }
+        }
 
         return new ListUtils(returnables);
     }
 
-    public <T> T reduce(T initial, Reducer<T, V> reducer){
-        for(V value : values)
+    public <T> T reduce(T initial, Reducer<T, V> reducer) {
+        for (V value : values) {
             initial = reducer.reduce(initial, value);
+        }
 
         return initial;
     }
 
-    public ListUtils<V> each(Action<V> action){
-        for(V value : values)
+    public ListUtils<V> each(Action<V> action) {
+        for (V value : values) {
             action.executeAction(value);
+        }
 
         return this;
     }
 
-    public ListUtils<V> eachIndexed(Action<MapEntry<Integer, V>> action){
-        for(int i = 0; i < values.size(); i++)
+    public ListUtils<V> eachIndexed(Action<MapEntry<Integer, V>> action) {
+        for (int i = 0; i < values.size(); i++) {
             action.executeAction(new MapEntry(i, values.get(i)));
+        }
 
         return this;
     }
 
-    public ListUtils<V> take(int count){
+    public ListUtils<V> take(int count) {
         return take(count, 0);
     }
 
-    public ListUtils<V> take(int count, int offset){
+    public ListUtils<V> take(int count, int offset) {
         List<V> returnables = new ArrayList<V>();
 
-        for(int i = offset; i < offset + count && i < values.size(); i++)
+        for (int i = offset; i < offset + count && i < values.size(); i++) {
             returnables.add(values.get(i));
+        }
 
         return new ListUtils(returnables);
     }
 
-    public <T, U> ListUtils<U> zipWith(List<T> subset, ListUtils.Zipper<V, T, U> zipper){
-        if(values.size() != subset.size())
+    public <T, U> ListUtils<U> zipWith(List<T> subset, ListUtils.Zipper<V, T, U> zipper) {
+        if (values.size() != subset.size()) {
             throw new IllegalArgumentException("Subset size [" + subset.size() + "] does not match values [" + values.size() + "]");
+        }
 
         List<U> returnables = new ArrayList<U>();
 
-        for(int i = 0; i < values.size(); i++)
+        for (int i = 0; i < values.size(); i++) {
             returnables.add(zipper.zip(values.get(i), subset.get(i)));
+        }
 
         return new ListUtils(returnables);
     }
 
-    public ListUtils<V> reverse(){
+    public ListUtils<V> reverse() {
         List<V> returnables = new ArrayList<V>();
 
-        for(int i = values.size() - 1; 0 <= i; i--)
+        for (int i = values.size() - 1; 0 <= i; i--) {
             returnables.add(values.get(i));
+        }
 
         return new ListUtils(returnables);
     }
 
-    public ListUtils<V> sort(Comparator<V> comparator){
+    public ListUtils<V> sort(Comparator<V> comparator) {
         List<V> returnables = new ArrayList<V>();
 
-        for(int i = 0; i < values.size(); i++)
+        for (int i = 0; i < values.size(); i++) {
             returnables.add(values.get(i));
+        }
 
-        Collections.sort(returnables,
-                comparator);
+        Collections.sort(
+            returnables,
+            comparator
+        );
 
         return new ListUtils(returnables);
     }
 
-    public ListUtils<V> unique(){
+    public ListUtils<V> unique() {
         List<V> returnables = new ArrayList<V>();
 
-        for(int i = 0; i < values.size(); i++)
-            if(!returnables.contains(values.get(i)))
+        for (int i = 0; i < values.size(); i++) {
+            if (!returnables.contains(values.get(i))) {
                 returnables.add(values.get(i));
+            }
+        }
 
         return new ListUtils(returnables);
     }
@@ -189,12 +214,12 @@ public class ListUtils<V> {
     public ListUtils<Map.Entry<String, List<V>>> groupBy(@NonNull Converter<V, String> groupHasher) {
         Map<String, List<V>> map = new HashMap<String, List<V>>();
 
-        for(V value : values) {
+        for (V value : values) {
             String hash = groupHasher.convert(value);
 
             List<V> values = map.get(hash);
 
-            if(values == null){
+            if (values == null) {
                 values = new ArrayList<V>();
 
                 map.put(hash, values);
@@ -205,27 +230,30 @@ public class ListUtils<V> {
 
         List<Map.Entry<String, List<V>>> returnables = new ArrayList<Map.Entry<String, List<V>>>();
 
-        for(String key : map.keySet())
+        for (String key : map.keySet()) {
             returnables.add(new MapEntry<String, List<V>>(key, map.get(key)));
+        }
 
         return new ListUtils(returnables);
     }
 
     public V first() {
-        if(values.size() < 1)
+        if (values.size() < 1) {
             return null;
+        }
 
         return values.get(0);
     }
 
     public V last() {
-        if(values.size() < 1)
+        if (values.size() < 1) {
             return null;
+        }
 
         return values.get(values.size() - 1);
     }
 
-    public String join(String delimiter){
+    public String join(String delimiter) {
         return join(delimiter, new Converter<V, String>() {
             public String convert(V from) {
                 return from.toString();
@@ -233,20 +261,21 @@ public class ListUtils<V> {
         });
     }
 
-    public String join(String delimiter, Converter<V, String> converter){
+    public String join(String delimiter, Converter<V, String> converter) {
         String joined = "";
 
-        if(0 < values.size()){
+        if (0 < values.size()) {
             joined += converter.convert(values.get(0));
 
-            for(int i = 1; i < values.size(); i++)
+            for (int i = 1; i < values.size(); i++) {
                 joined += delimiter + converter.convert(values.get(i));
+            }
         }
 
         return joined;
     }
 
-    public List<V> values(){
+    public List<V> values() {
         return values;
     }
 
@@ -256,29 +285,32 @@ public class ListUtils<V> {
      * items to determine the true array type.
      */
     @SuppressWarnings("unchecked")
-    public V[] valuesToArray(Class<V> arrayType){
-        return values.toArray((V[]) Array.newInstance(arrayType,
-                values.size()));
+    public V[] valuesToArray(Class<V> arrayType) {
+        return values.toArray((V[]) Array.newInstance(
+            arrayType,
+            values.size()
+        ));
     }
 
-    public static <T> ListUtils<T> fromMappedCount(int count, Converter<Integer, T> converter){
+    public static <T> ListUtils<T> fromMappedCount(int count, Converter<Integer, T> converter) {
         List<T> values = new ArrayList<T>();
 
-        for(int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++) {
             values.add(converter.convert(i));
+        }
 
         return new ListUtils<T>(values);
     }
 
-    public static <T> ListUtils<T> from(@NonNull T[] values){
+    public static <T> ListUtils<T> from(@NonNull T[] values) {
         return new ListUtils<T>(values);
     }
 
-    public static <T> ListUtils<T> from(@NonNull List<T> values){
+    public static <T> ListUtils<T> from(@NonNull List<T> values) {
         return new ListUtils<T>(values);
     }
 
-    public static <T> ListUtils<T> from(@NonNull Set<T> values){
+    public static <T> ListUtils<T> from(@NonNull Set<T> values) {
         return new ListUtils<T>(values);
     }
 
--- collection-utils-legacy/src/test/java/com/guardanis/collections/legacy/tools/ListUtilsTests.java
+++ collection-utils-legacy/src/test/java/com/guardanis/collections/legacy/tools/ListUtilsTests.java
@@ -15,9 +15,9 @@ public class ListUtilsTests {
         String target = "a, b, c, d";
 
         String actual = ListUtils.from(original.split(","))
-                .join(", ");
+            .join(", ");
 
-        assert(actual.equals(target));
+        assert (actual.equals(target));
     }
 
     @Test
@@ -25,20 +25,20 @@ public class ListUtilsTests {
         String original = "1,2,3,4,5,6,7";
 
         int reduced = ListUtils.from(original.split(","))
-                .map(new SimpleNumberConverter()) // 1,2,3,4,5,6,7
-                .filter(new ListUtils.Filter<Integer>() {
-                    public boolean isFilterMatched(Integer obj) {
-                        return obj > 1 && obj < 7;
-                    }
-                }) // 2,3,4,5,6
-                .take(4)  // 2,3,4,5
-                .reduce(0, new ListUtils.Reducer<Integer, Integer>() {
-                    public Integer reduce(Integer last, Integer value) {
-                        return last + value;
-                    }
-                });
+            .map(new SimpleNumberConverter()) // 1,2,3,4,5,6,7
+            .filter(new ListUtils.Filter<Integer>() {
+                public boolean isFilterMatched(Integer obj) {
+                    return obj > 1 && obj < 7;
+                }
+            }) // 2,3,4,5,6
+            .take(4)  // 2,3,4,5
+            .reduce(0, new ListUtils.Reducer<Integer, Integer>() {
+                public Integer reduce(Integer last, Integer value) {
+                    return last + value;
+                }
+            });
 
-        assert(reduced == 14);
+        assert (reduced == 14);
     }
 
     @Test
@@ -46,15 +46,15 @@ public class ListUtilsTests {
         String original = "1,1,1,2,2,3,3,3,4,4";
 
         int reduced = ListUtils.from(original.split(","))
-                .unique() // "1,2,3,4"
-                .map(new SimpleNumberConverter()) // 1,2,3,4
-                .reduce(0, new ListUtils.Reducer<Integer, Integer>() {
-                    public Integer reduce(Integer last, Integer value) {
-                        return last + value;
-                    }
-                }); // 10
+            .unique() // "1,2,3,4"
+            .map(new SimpleNumberConverter()) // 1,2,3,4
+            .reduce(0, new ListUtils.Reducer<Integer, Integer>() {
+                public Integer reduce(Integer last, Integer value) {
+                    return last + value;
+                }
+            }); // 10
 
-        assert(reduced == 10);
+        assert (reduced == 10);
     }
 
     @Test
@@ -62,30 +62,32 @@ public class ListUtilsTests {
         String original = "4,3,2,1,7,6,5";
 
         String joined = ListUtils.from(original.split(","))
-                .map(new SimpleNumberConverter()) // 4,3,2,1,7,6,5
-                .sort(new Comparator<Integer>() {
-                    public int compare(Integer first, Integer second) {
-                        return first - second;
-                    }
-                }) // 1,2,3,4,5,6,7
-                .reverse() // 7,6,5,4,3,2,1
-                .join(""); // 7654321
+            .map(new SimpleNumberConverter()) // 4,3,2,1,7,6,5
+            .sort(new Comparator<Integer>() {
+                public int compare(Integer first, Integer second) {
+                    return first - second;
+                }
+            }) // 1,2,3,4,5,6,7
+            .reverse() // 7,6,5,4,3,2,1
+            .join(""); // 7654321
 
-        assert(joined.equals("7654321"));
+        assert (joined.equals("7654321"));
     }
 
     @Test
     public void testValuesToArrayReflection() throws Exception {
-        Integer[] values = ListUtils.fromMappedCount(3,
+        Integer[] values = ListUtils.fromMappedCount(
+                3,
                 new ListUtils.Converter<Integer, Integer>() {
                     public Integer convert(Integer from) {
                         return from;
                     }
-                }) // 0,1,2
-                .valuesToArray(Integer.class);
+                }
+            ) // 0,1,2
+            .valuesToArray(Integer.class);
 
-        assert(values[0] == 0
-                && values[2] == 2);
+        assert (values[0] == 0
+            && values[2] == 2);
     }
 
     @Test
@@ -105,19 +107,19 @@ public class ListUtilsTests {
         parent.add(child2);
 
         String value = ListUtils.from(parent)
-                .flatMap(new ListUtils.Converter<Object, Integer>() {
-                    public Integer convert(Object from) {
-                        return (Integer) from;
-                    }
-                })
-                .reduce("", new ListUtils.Reducer<String, Integer>() {
-                    public String reduce(String last, Integer current) {
-                        return last + current + " ";
-                    }
-                })
-                .trim();
+            .flatMap(new ListUtils.Converter<Object, Integer>() {
+                public Integer convert(Object from) {
+                    return (Integer) from;
+                }
+            })
+            .reduce("", new ListUtils.Reducer<String, Integer>() {
+                public String reduce(String last, Integer current) {
+                    return last + current + " ";
+                }
+            })
+            .trim();
 
-        assert(value.equals("0 10 11 12 20 21"));
+        assert (value.equals("0 10 11 12 20 21"));
     }
 
     @Test
@@ -136,19 +138,19 @@ public class ListUtilsTests {
         parent.add(child2);
 
         String value = ListUtils.from(parent)
-                .flatMap(new ListUtils.Converter<Integer, Integer>() {
-                    public Integer convert(Integer from) {
-                        return from;
-                    }
-                })
-                .reduce("", new ListUtils.Reducer<String, Integer>() {
-                    public String reduce(String last, Integer current) {
-                        return last + current + " ";
-                    }
-                })
-                .trim();
+            .flatMap(new ListUtils.Converter<Integer, Integer>() {
+                public Integer convert(Integer from) {
+                    return from;
+                }
+            })
+            .reduce("", new ListUtils.Reducer<String, Integer>() {
+                public String reduce(String last, Integer current) {
+                    return last + current + " ";
+                }
+            })
+            .trim();
 
-        assert(value.equals("10 11 12 20 21"));
+        assert (value.equals("10 11 12 20 21"));
     }
 
     @Test
@@ -160,41 +162,41 @@ public class ListUtilsTests {
         parent.add(3);
 
         String value = ListUtils.from(parent)
-                .flatMap(new ListUtils.Converter<Integer, Integer>() {
-                    public Integer convert(Integer from) {
-                        return from < 2 ? null : from;
-                    }
-                })
-                .reduce("", new ListUtils.Reducer<String, Integer>() {
-                    public String reduce(String last, Integer current) {
-                        return last + current + " ";
-                    }
-                })
-                .trim();
+            .flatMap(new ListUtils.Converter<Integer, Integer>() {
+                public Integer convert(Integer from) {
+                    return from < 2 ? null : from;
+                }
+            })
+            .reduce("", new ListUtils.Reducer<String, Integer>() {
+                public String reduce(String last, Integer current) {
+                    return last + current + " ";
+                }
+            })
+            .trim();
 
-        assert(value.equals("2 3"));
+        assert (value.equals("2 3"));
     }
 
     @Test
     public void testGroupBy() throws Exception {
-        String[] data = new String[]{ "1", "2", "1" };
+        String[] data = new String[]{"1", "2", "1"};
 
         int size = ListUtils.from(data)
-                .groupBy(new ListUtils.Converter<String, String>() {
-                    public String convert(String item) {
-                        return item;
-                    }
-                })
-                .filter(new ListUtils.Filter<Map.Entry<String, List<String>>>() {
-                    public boolean isFilterMatched(Map.Entry<String, List<String>> items) {
-                        return items.getKey().equals("1");
-                    }
-                })
-                .first()
-                .getValue()
-                .size();
+            .groupBy(new ListUtils.Converter<String, String>() {
+                public String convert(String item) {
+                    return item;
+                }
+            })
+            .filter(new ListUtils.Filter<Map.Entry<String, List<String>>>() {
+                public boolean isFilterMatched(Map.Entry<String, List<String>> items) {
+                    return items.getKey().equals("1");
+                }
+            })
+            .first()
+            .getValue()
+            .size();
 
-        assert(size == 2);
+        assert (size == 2);
     }
 
     private static class SimpleNumberConverter implements ListUtils.Converter<String, Integer> {
